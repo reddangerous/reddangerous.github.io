d64285e8659b493ad4dd0e157c1dd2b56723881f
David Kinyanjui <dkinyanjui144@gmail.com>
2022-06-10 10:06:57 +0300

    potfolio website

---
+++ Script.js
@@ -0,0 +1,31 @@
+console.log("its working");
+let theme = localStorage.getItem('theme')
+if(theme==null){
+  setTheme('light')
+}else{
+  setTheme(theme)
+}
+
+let themDots = document.getElementsByClassName('Theme-dot')
+
+for (var i=0;themDots.length >i; i++){
+  themDots[i].addEventListener('click',function () {
+    let mode = this.dataset.mode
+    setTheme(mode)
+  })
+}
+function setTheme(mode){
+  if(mode=='light'){
+    document.getElementById('theme-style').href='default.css';
+  }
+  if(mode=='blue'){
+    document.getElementById('theme-style').href='Blue.css';
+  }
+  if(mode=='green'){
+    document.getElementById('theme-style').href='Green.css';
+  }
+  if(mode=='purple'){
+    document.getElementById('theme-style').href='Purple.css';
+  }
+  localStorage.setItem('theme',mode)
+}
